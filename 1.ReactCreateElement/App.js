4cce22b5b3afc6dea9551319ff99de2e4e400e69
Raghav Verma <raghav2103rv@gmail.com>
2025-07-20 21:18:44 +0530

    3-restrcutred the proejct files

--- 1.ReactCreateElement/App.js
+++ 1.ReactCreateElement/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 /**
  *
  * <div id="parent">
@@ -18,16 +18,16 @@ import ReactDOM from "react-dom";
  */
 // const parent = React.createElement("tag",{attribute},children(if multiple children pass as an array))
 
-const parent = React.createElement("div",{id: "parent",},[
-    React.createElement("div",{id: "child",},[
-        React.createElement("h1", {}, "I'm h1 tag"),
-        React.createElement("h2", {}, "I'm h2 tag"),
-      ]),
-    React.createElement("div",{id: "child2",},[
-        React.createElement("h1", {}, "I'm h1 tag"),
-        React.createElement("h2", {}, "I'm h2 tag"),
-      ]),
-  ]);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child", key: "child-1" }, [
+    React.createElement("h1", { key: "h1-1" }, "I'm h1 tag"),
+    React.createElement("h2", { key: "h2-1" }, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2", key: "child-2" }, [
+    React.createElement("h1", { key: "h1-2" }, "I'm h1 tag"),
+    React.createElement("h2", { key: "h2-2" }, "I'm h2 tag"),
+  ]),
+]);
 
 // const heading = React.createElement(
 //   "h1",
@@ -36,5 +36,5 @@ const parent = React.createElement("div",{id: "parent",},[
 // );
 // console.log(heading); //object
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); // replaces whatever is inside the root
+const root = createRoot(document.getElementById("root"));
+root.render(parent);
